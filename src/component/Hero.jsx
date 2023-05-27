7b1b44d29c0227ff9c400cd98681b84ddba61473
Jol0o <jloyd9836@gmail.com>
2023-05-27 12:34:24 +0800

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,18 +1,24 @@
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import React, { useState } from "react";
 import { AiFillFacebook } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
 import { BsArrowDown } from "react-icons/bs";
 
-const Hero = ({ isVisible }) => {
+const Hero = () => {
   const scrollToContent = () => {
     const contentElement = document.getElementById("project");
     contentElement.scrollIntoView({ behavior: "smooth" });
   };
 
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
-    <div className="h-screen w-full pt-3 text-white font-mono relative bg-[url('/world.png')] inset-0  bg-cover bg-center">
+    <div
+      ref={ref}
+      className="h-screen w-full pt-3 text-white font-mono relative bg-[url('/world.png')] inset-0  bg-cover bg-center"
+    >
       <ul className="hidden lg:flex flex-col top-[45%] transition fixed left-[0] gap-1 uppercase font-semibold text-2xl z-10 text-[#9C9C9C]">
         <li className="ml-[-109px] hover:ml-[5px]  ease-out duration-300 hover:text-cyan-500 ">
           <a
@@ -45,55 +51,55 @@ const Hero = ({ isVisible }) => {
           </a>
         </li>
       </ul>
-      {isVisible && (
-        <div className="relative bg-opacity-75 bg-[#090E16] flex justify-center items-center pt-24 md:pt-0 h-full w-full flex-col ">
-          <div className="absolute w-auto top-[23%] flex justify-center items-center">
-            <motion.h2
-              transition={{
-                delay: 0.5,
-                duration: 0.8,
-                ease: [0, 0.71, 0.2, 1.01],
-              }}
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0 }}
-              className="text-center w-fit font-bold font-mono text-[36px] whitespace-nowrap sm:text-[90px] md:text-[110px] lg:text-[140px] xl:text-[190px] uppercase"
-            >
-              Web Developer.
-            </motion.h2>
-          </div>
-          <div className="absolute md:top-[23%]">
-            <motion.img
-              transition={{
-                delay: 0.5,
-                duration: 0.8,
-                ease: [0, 0.71, 0.2, 1.01],
-              }}
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0 }}
-              src="./avatar.png"
-              alt="avatar"
-              className=" rounded-full bg-gradient-to-b from-[#13a049a9] to-[#45d8d85e] shadow-lg  shadow-[#13a04968] h-80 w-80 md:w-[400px] md:h-[400px] "
-            />
-            <motion.div
-              transition={{
-                delay: 0.5,
-                duration: 0.8,
-                ease: [0, 0.71, 0.2, 1.01],
-              }}
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0 }}
-              className="flex items-center cursor-auto justify-center mt-10"
-            >
-              <button className="text-green-500 text-xl">
-                Let's get Started.
-              </button>
-            </motion.div>
-          </div>
+
+      <div className="relative bg-opacity-75 bg-[#090E16] flex justify-center items-center pt-24 md:pt-0 h-full w-full flex-col ">
+        <div className="absolute w-auto top-[23%] flex justify-center items-center">
+          <motion.h2
+            transition={{
+              delay: 0.5,
+              duration: 0.8,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0 }}
+            className="text-center w-fit font-bold font-mono text-[36px] whitespace-nowrap sm:text-[90px] md:text-[110px] lg:text-[140px] xl:text-[190px] uppercase"
+          >
+            Web Developer.
+          </motion.h2>
         </div>
-      )}
+        <div className="absolute md:top-[23%]">
+          <motion.img
+            transition={{
+              delay: 0.5,
+              duration: 0.8,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0 }}
+            src="./avatar.png"
+            alt="avatar"
+            className=" rounded-full bg-gradient-to-b from-[#13a049a9] to-[#45d8d85e] shadow-lg  shadow-[#13a04968] h-80 w-80 md:w-[400px] md:h-[400px] "
+          />
+          <motion.div
+            transition={{
+              delay: 0.5,
+              duration: 0.8,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex items-center cursor-auto justify-center mt-10"
+          >
+            <button className="text-green-500 text-xl">
+              Let's get Started.
+            </button>
+          </motion.div>
+        </div>
+      </div>
+
       <div className="z-0 animate-bounce absolute left-1/2 transform -translate-x-1/2 top-[90%] rounded-full hover:bg-green-400 transition bg-[#3F8E00] py-3 px-3">
         <button onClick={scrollToContent}>
           <BsArrowDown />
