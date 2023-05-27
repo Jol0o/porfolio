6358408adc5c0bb5826bc82af7a861e113d8ce74
Jol0o <jloyd9836@gmail.com>
2023-05-27 13:17:48 +0800

    Update Projects.jsx

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -56,7 +56,7 @@ const Projects = () => {
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
-                duration: 0.3,
+                duration: 0.7,
                 ease: [0, 0.71, 0.2, 1.01],
                 scale: {
                   type: "spring",
@@ -86,7 +86,7 @@ const Projects = () => {
                         initial={{ opacity: 0, x: -20 }}
                         animate={{ opacity: 1, x: 0, rotate: [0, 10, 0] }}
                         transition={{
-                          duration: 0.3,
+                          duration: 0.7,
                           ease: [0, 0.71, 0.2, 1.01],
                           scale: {
                             type: "spring",
@@ -105,7 +105,7 @@ const Projects = () => {
                         initial={{ opacity: 0, y: 20 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{
-                          duration: 0.3,
+                          duration: 0.7,
                           ease: [0, 0.71, 0.2, 1.01],
                           scale: {
                             type: "spring",
