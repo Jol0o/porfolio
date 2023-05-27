90bc545e600d0c79a2b83a1adcee0eea3e399262
Jol0o <jloyd9836@gmail.com>
2023-05-27 21:51:18 +0800

    Update Timeline.jsx

--- src/component/Timeline.jsx
+++ src/component/Timeline.jsx
@@ -39,7 +39,7 @@ const timelineVariants = {
       ease: [0, 0.71, 0.2, 1.01],
       scale: {
         type: "spring",
-        damping: 5,
+        damping: 3,
         stiffness: 100,
         restDelta: 0.001,
       },
@@ -88,6 +88,30 @@ export default function Timeline() {
               </p>
             </motion.div>
           </li>
+          <li className="relative flex items-baseline gap-6 pb-5">
+            <div className="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-green-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="12"
+                className="bi bi-circle-fill fill-green-500"
+                viewBox="0 0 16 16"
+              >
+                <circle cx="8" cy="8" r="8" />
+              </svg>
+            </div>
+            <motion.div variants={timelineVariants}>
+              <h1 className="font-semibold text-md text-white">
+                Responsive Design
+              </h1>
+              <p className="text-sm text-gray-400">12-8-2022</p>
+              <p className="mt-2 text-gray-400 text-sm">
+                Learn how to create responsive layouts that adapt to different
+                screen sizes, using tools such as media queries and flexbox.
+              </p>
+            </motion.div>
+          </li>
+
           {/* Add the remaining list items */}
         </ul>
       </motion.div>
