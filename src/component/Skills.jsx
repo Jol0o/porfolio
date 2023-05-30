dd4419b6767c565ed55a91294edc4a1fc6f19f9b
Jol0o <jloyd9836@gmail.com>
2023-05-30 09:57:28 +0800

    Update Skills.jsx

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -16,27 +16,20 @@ export default function Skills() {
   return (
     <Section>
       <div className="w-full min-h-[50vh] flex flex-col items-center justify-center mt-[3%]">
-        <div
-          variants={topVariants}
-          className="text-green-400 font-bold bg-green-900 w-fit rounded-lg px-2"
-        >
+        <div className="text-green-400 font-bold bg-green-900 w-fit rounded-lg px-2">
           🧑‍💻 Skill & Experience
         </div>
         <div className="flex items-center justify-center flex-col">
-          <h1
-            variants={topVariants}
-            className="my-4 font-bold text-center text-[40px] text-white"
-          >
+          <h1 className="my-4 font-bold text-center text-[40px] text-white">
             Technologies and skills
           </h1>
-          <p variants={topVariants} className="mb-4 text-gray-500 text-sm">
+          <p className="mb-4 text-gray-500 text-sm">
             Techs I use on a daily basis.
           </p>
           <div className="flex gap-10 justify-center items-center px-[3%] flex-wrap">
             {skills.map((img) => {
               return (
                 <img
-                  variants={bottomVariants}
                   src={img}
                   alt="img"
                   className="h-10  hover:scale-125 transition "
