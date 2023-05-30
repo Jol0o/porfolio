4bad8fa8635a872fde8ad1646381cd8487673e5d
Jol0o <jloyd9836@gmail.com>
2023-05-30 10:00:07 +0800

    Update Section.jsx

--- src/component/Section.jsx
+++ src/component/Section.jsx
@@ -1,21 +1,23 @@
 import { useRef } from "react";
-import { useInView } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 
 export default function Section({ children }) {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  const [isInView, entry] = useInView({ triggerOnce: true });
 
   return (
     <section ref={ref}>
-      <span
+      <motion.span
+        initial={{ transform: "translateX(-200px)", opacity: 0 }}
+        animate={{ transform: "none", opacity: 1 }}
+        transition={{ duration: 0.9, delay: 0.5, ease: [0.17, 0.55, 0.55, 1] }}
         style={{
           transform: isInView ? "none" : "translateX(-200px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
         }}
       >
         {children}
-      </span>
+      </motion.span>
     </section>
   );
 }
