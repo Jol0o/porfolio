7f9a41d65e2d28907bdcc56a15d7e9cc2f7c4104
Jol0o <jloyd9836@gmail.com>
2023-05-30 10:26:44 +0800

    Update Skills.jsx

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,18 +1,35 @@
 import { motion, useInView } from "framer-motion";
-import { useRef } from "react";
+import React from "react";
+
+const cardVariants = {
+  offscreen: {
+    y: 300,
+  },
+  onscreen: {
+    y: 50,
+    rotate: -10,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8,
+    },
+  },
+};
+
+function Card({ children }) {
+  return (
+    <motion.div
+      className="card-container"
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <motion.div variants={cardVariants}>{children}</motion.div>
+    </motion.div>
+  );
+}
 
 export default function Skills() {
-  const ref = useRef(null);
-  const { inView, entry } = useInView({
-    triggerOnce: true,
-    threshold: 0.1, // Adjusted threshold value
-  });
-
-  const variants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0 },
-  };
-
   const skills = [
     "/Skill/Github.png",
     "/Skill/Group 1.png",
@@ -24,36 +41,31 @@ export default function Skills() {
     "/Skill/Vector.png",
   ];
   return (
-    <motion.div
-      initial="hidden"
-      animate={inView ? "visible" : "hidden"}
-      variants={variants}
-      transition={{ duration: 0.5 }}
-      ref={ref}
-      className="w-full min-h-[50vh] flex flex-col items-center justify-center mt-[3%]"
-    >
-      <div className="text-green-400 font-bold bg-green-900 w-fit rounded-lg px-2">
-        🧑‍💻 Skill & Experience
-      </div>
-      <div className="flex items-center justify-center flex-col">
-        <h1 className="my-4 font-bold text-center text-[40px] text-white">
-          Technologies and skills
-        </h1>
-        <p className="mb-4 text-gray-500 text-sm">
-          Techs I use on a daily basis.
-        </p>
-        <div className="flex gap-10 justify-center items-center px-[3%] flex-wrap">
-          {skills.map((img) => {
-            return (
-              <img
-                src={img}
-                alt="img"
-                className="h-10  hover:scale-125 transition "
-              />
-            );
-          })}
+    <Card>
+      <div className="w-full min-h-[50vh] flex flex-col items-center justify-center mt-[3%]">
+        <div className="text-green-400 font-bold bg-green-900 w-fit rounded-lg px-2">
+          🧑‍💻 Skill & Experience
+        </div>
+        <div className="flex items-center justify-center flex-col">
+          <h1 className="my-4 font-bold text-center text-[40px] text-white">
+            Technologies and skills
+          </h1>
+          <p className="mb-4 text-gray-500 text-sm">
+            Techs I use on a daily basis.
+          </p>
+          <div className="flex gap-10 justify-center items-center px-[3%] flex-wrap">
+            {skills.map((img) => {
+              return (
+                <img
+                  src={img}
+                  alt="img"
+                  className="h-10  hover:scale-125 transition "
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
-    </motion.div>
+    </Card>
   );
 }
