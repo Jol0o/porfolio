64560555adcf541d239858a7eddd8a9f8f187af4
Jol0o <jloyd9836@gmail.com>
2023-05-30 10:35:37 +0800

    Update Skills.jsx

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -47,29 +47,31 @@ export default function Skills() {
     "/Skill/Vector.png",
   ];
   return (
-    <Card className="w-full min-h-[50vh] flex flex-col items-center justify-center mt-[3%]">
-      <div className="text-green-400 font-bold bg-green-900 w-fit rounded-lg px-2">
-        🧑‍💻 Skill & Experience
-      </div>
-      <div className="flex items-center justify-center flex-col">
-        <h1 className="my-4 font-bold text-center text-[40px] text-white">
-          Technologies and skills
-        </h1>
-        <p className="mb-4 text-gray-500 text-sm">
-          Techs I use on a daily basis.
-        </p>
-        <div className="flex gap-10 justify-center items-center px-[3%] flex-wrap">
-          {skills.map((img) => {
-            return (
-              <img
-                src={img}
-                alt="img"
-                className="h-10  hover:scale-125 transition "
-              />
-            );
-          })}
+    <div className="w-full min-h-[50vh] flex flex-col items-center justify-center mt-[3%]">
+      <Card>
+        <div className="text-green-400 font-bold bg-green-900 w-fit rounded-lg px-2">
+          🧑‍💻 Skill & Experience
         </div>
-      </div>
-    </Card>
+        <div className="flex items-center justify-center flex-col">
+          <h1 className="my-4 font-bold text-center text-[40px] text-white">
+            Technologies and skills
+          </h1>
+          <p className="mb-4 text-gray-500 text-sm">
+            Techs I use on a daily basis.
+          </p>
+          <div className="flex gap-10 justify-center items-center px-[3%] flex-wrap">
+            {skills.map((img) => {
+              return (
+                <img
+                  src={img}
+                  alt="img"
+                  className="h-10  hover:scale-125 transition "
+                />
+              );
+            })}
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 }
